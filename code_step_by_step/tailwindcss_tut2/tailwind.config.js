e2944d4bbbe056260d9b2d3f1b695a05e4dc5d5a
Aaditya Kumar Mittal <adityakumarashish1909@gmail.com>
2025-02-23 23:52:54 +0530

    feat: Completed Flex, Grid, Transitions, Animations and Screen Breakpoints

--- code_step_by_step/tailwindcss_tut2/tailwind.config.js
+++ code_step_by_step/tailwindcss_tut2/tailwind.config.js
@@ -2,6 +2,17 @@
 module.exports = {
   content: ["*"],
   theme: {
+    screens: {
+      fourkt: "400px",
+      fivekt: "500px",
+      sixkt: "600px",
+      sevenkt: "700px",
+      eightkt: "800px",
+      ninekt: "900px",
+      tenkt: "1000px",
+      elevenkt: "1100px",
+      twelvekt: "1200px",
+    },
     extend: {},
   },
   plugins: [],
